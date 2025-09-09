d3865cb2b11ed5171e7c08ec51b326016b698d26
FTI-LAB03-PC13 <FTI-LAB03-PC13@FTI-LAB03-PC13>
2025-09-09 11:58:30 +0700

    praktikum 1 kelas C

---
+++ prakalpro_2025_C_2511531019/src/pekan1/Hello_World.java
@@ -0,0 +1,20 @@
+package pekan1;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//First Program
+		/* komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 praktikum alpro
+		 */
+		System.out.println("Hello_World");
+		System.out.println();
+		System.out.println("Program ini menghasilkan");
+		System.out.println("Empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("This" + "program prints a \n" + "quote from the Gettysburg Address.");
+	}
+}
---
+++ prakalpro_2025_C_2511531019/src/pekan1/Membuat_Kue.java
@@ -0,0 +1,26 @@
+package pekan1;
+
+public class Membuat_Kue {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		makeBatter();
+		memanggang();
+		menghias();
+	}
+	public static void makeBatter() {
+		System.out.println("Campur bahan kue kering");
+		System.out.println("Krim mentega dan gula");
+		System.out.println("Aduk telurnya");
+		System.out.println("Masukkan bahan kue kering");
+	}
+	public static void memanggang() {
+		System.out.println("Setel suhu oven");
+		System.out.println("Setel pengatur waktu");
+		System.out.println("Masukkan kue kering ke dalam oven");
+		System.out.println("Biarkan biskuit untuk dipanggang");
+	}
+	public static void menghias() {
+		System.out.println("Campur bahan untuk di-frosting");
+		System.out.println("Taburkan frosting dan taburan");
+	}
+}
