9597cd5a37153103ee430f0b3b1229e996d1d7df
FTI-LAB03-PC13 <FTI-LAB03-PC13@FTI-LAB03-PC13>
2025-09-16 11:37:58 +0700

    Praktikum_2_kelas_C

---
+++ prakalpro_2025_C_2511531019/src/pekan2/DeklarasiVariabel.java
@@ -0,0 +1,21 @@
+package pekan2;
+public class DeklarasiVariabel {
+	/* program java
+	 *latihan
+	 *tentang pendeklarasian variabel
+	 */
+	static int umur=25;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kode;
+		boolean isDibawahUmur;
+		kode = 1234;
+		double gaji;
+		gaji = 5500000.23;
+		isDibawahUmur = true;
+		System.out.println("Status: "+isDibawahUmur);
+		System.out.println("Kode: "+kode);
+		System.out.println("Umur: "+umur);
+		System.out.println("Gaji: "+gaji);
+	}
+}
---
+++ prakalpro_2025_C_2511531019/src/pekan2/KelilingLingkaran.java
@@ -0,0 +1,9 @@
+package pekan2;
+public class KelilingLingkaran {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+		double radius = 30;
+		System.out.println("Keliling = " + 2 * PI * radius);
+	}
+}
