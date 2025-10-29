4ce96c034367f0d7ec740086abbefa3b704f35fc
FTI-LAB03-PC13 <FTI-LAB03-PC13@FTI-LAB03-PC13>
2025-10-29 16:40:15 +0700

    praktikumpekan4kelasc

---
+++ prakalpro_2025_C_2511531019/src/pekan5_2511531019/PerulanganFor2.java
@@ -0,0 +1,9 @@
+package pekan5_2511531019;
+public class PerulanganFor2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
---
+++ prakalpro_2025_C_2511531019/src/pekan5_2511531019/nestedfor0.java
@@ -0,0 +1,13 @@
+package pekan5_2511531019;
+public class nestedfor0 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int line = 1; line <= 5; line++) {
+			for (int j = 1; j <= (-1 * line + 5); j++) {
+				System.out.print(".");
+			}
+			System.out.print(line);
+			System.out.println();
+		}
+	}
+}
